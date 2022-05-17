38964b527d021ac4902a129923af4de7171109b6
juanfril <juanfril@gmail.com>
2022-05-17 20:16:06 +0200

    fix: approach good use for dinamic class

--- components/UI/SubmitButton.tsx
+++ components/UI/SubmitButton.tsx
@@ -3,10 +3,10 @@ type Props = {
   color?: string;
 };
 
-export const SubmitButton = ({ color = 'bg-indigo', text }: Props) => {
+export const SubmitButton = ({ color = 'bg-indigo-600', text }: Props) => {
   return (
     <button
-      className={`${color}-600 hover:${color}-700 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+      className={`${color} hover:bg-opacity-75 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
     >
       {text}
     </button>
