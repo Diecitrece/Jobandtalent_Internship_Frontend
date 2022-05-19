68541519ee2bd887f9aca650bf1dabba36c334ee
juanfril <juanfril@gmail.com>
2022-05-19 19:57:28 +0200

    refactor: add InputState model UserRegister

--- lib/models/user.tsx
+++ lib/models/user.tsx
@@ -1,11 +1,13 @@
+type InputState = { value: string; error: string };
+
 export type UserRegister = {
-  firstName: { value: string; error: string };
-  surNames: { value: string; error: string };
-  email: { value: string; error: string };
-  password: { value: string; error: string };
-  confirmPassword: { value: string; error: string };
-  phone: { value: string; error: string };
-  address: { value: string; error: string };
+  firstName: InputState;
+  surNames: InputState;
+  email: InputState;
+  password: InputState;
+  confirmPassword: InputState;
+  phone: InputState;
+  address: InputState;
 };
 
 export type UserCredentials = {
