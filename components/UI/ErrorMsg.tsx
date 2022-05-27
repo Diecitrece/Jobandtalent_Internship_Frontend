c5f3109d8f55e8ce914214d9f3ad87cec266096c
juanfril <juanfril@gmail.com>
2022-05-27 22:26:00 +0200

    some errors found instead be warned

--- components/UI/ErrorMsg.tsx
+++ components/UI/ErrorMsg.tsx
@@ -9,7 +9,7 @@ export const ErrorMsg = ({ message }: Props) => {
         className='bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4'
         role='alert'
       >
-        <p className='font-bold'>Be Warned</p>
+        <p className='font-bold'>Some errors found</p>
         <p>{message}</p>
       </div>
     </>
