765ceca6740b19dd2bbadb782b8a7105635f32fc
juanfril <juanfril@gmail.com>
2022-05-30 12:43:22 +0200

    quit meta secure policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Job and Talent</title>
-        <meta
-          httpEquiv='Content-Security-Policy'
-          content='upgrade-insecure-requests'
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
