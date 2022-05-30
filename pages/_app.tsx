f73cc7520a9c0aa262ff19faf60a43c885cab68e
juanfril <juanfril@gmail.com>
2022-05-30 12:35:11 +0200

    add meta secure policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Job and Talent</title>
+        <meta
+          httpEquiv='Content-Security-Policy'
+          content='upgrade-insecure-requests'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
